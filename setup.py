35fb344e51e9bbe6ef16ffa6f9ec2fb78ecfb96d
Amit Upadhyay <comments@amitu.com>
2011-12-12 04:58:34 -0500

    setup.py to reflect new structure

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name = "amitu-zutils",
-    version = "0.1.1",
-    url = 'http://packages.python.org/amitu-zutils/',
-    license = 'BSD',
     description = "Generic Utilities for ZMQ based services",
+    version = "0.1.2",
     author = 'Amit Upadhyay',
     author_email = "upadhyay@gmail.com",
-    py_modules = [
-        "amitu.zutils", "amitu.zidgen", "amitu.zconfig", "amitu.zqueue"
-    ],
+
+    url = 'http://packages.python.org/amitu-zutils/',
+    license = 'BSD',
+
     namespace_packages = ["amitu"],
+    packages = find_packages(),
 )
